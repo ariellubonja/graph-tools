76decb3c0a6bb1c71faa12350412888e971216fa
Ariel Lubonja <ariel@cs.jhu.edu>
2024-01-29 15:45:58 -0500

    Add input path for ligra dir

--- creation/ligra_adjgraph.sh
+++ creation/ligra_adjgraph.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Prompt the user to enter the root directory
-echo "Enter the path to the directory of .edgelist files:"
-read root_directory
+# Prompt the user to enter the path to the Ligra directory
+echo "Enter the path to the Ligra repository base directory:"
+read ligra_directory
+
+# Prompt the user to enter the path to the Erdos-Renyi graph directory
+echo "Enter the path to the Erdos-Renyi graph directory:"
+read erdos_renyi_directory
 
 # Define the output directory
-output_directory="${root_directory}/Ligra-AdjGraphs"
-
-# Change to the root directory
-cd "$root_directory" || exit
+output_directory="${erdos_renyi_directory}/Ligra-AdjGraphs"
 
 # Compile ligra/utils if not already compiled
-make -j ligra/utils
+(cd "$ligra_directory" && make -j utils)
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_directory"
 
-# Loop over all .edgelist files in the root directory
-for file in *.edgelist; do
+# Loop over all .edgelist files in the Erdos-Renyi graph directory
+for file in "${erdos_renyi_directory}"/*.edgelist; do
     # Define the output file name by replacing the extension and adding the output directory
-    output_file="${output_directory}/${file%.edgelist}.AdjGraph"
+    output_file="${output_directory}/$(basename "${file%.edgelist}").AdjGraph"
 
     # Run the SNAPtoAdj utility
-    ./SNAPtoAdj "$file" "$output_file"
+    "${ligra_directory}/utils/SNAPtoAdj" "$file" "$output_file"
 done
